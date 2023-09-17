2fba4b9ce883265f07b69e77b1afbdd60a80d567
Guilherme Reis <guilhermejosereis27@gmail.com>
2023-09-16 20:15:42 -0400

    Export all functions

--- index.ts
+++ index.ts
@@ -2,19 +2,4 @@ import { brl } from "./core/currency/currency";
 import { zip_code } from "./core/zip_code/zip_code";
 import { date_time, date } from "./core/date/date";
 
-console.log(brl(123.223));
-console.log(brl("123.223"));
-console.log(brl("12GS3.22a3"));
-
-console.log(zip_code(78008435, true));
-console.log(zip_code(78008435));
-
-console.log(zip_code("78008435", true));
-console.log(zip_code("78008435"));
-
-console.log(zip_code("78008VVVV1435", true));
-console.log(zip_code("z78008zzz435e"));
-
-console.log(date_time("2023-09-15 21:19:20.909-04"));
-console.log(date_time("2023-09-15 21:19:20"));
-console.log(date("2023-09-15 21:19:20.909-04"));
+export { brl, zip_code, date_time, date };
